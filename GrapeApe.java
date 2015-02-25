af7688bc58282921f466ade22f071a7522db425a
csickler <csickler@macmini-lab11.local>
2015-02-25 11:01:03 -0500

    created farm

---
+++ GrapeApe.java
@@ -0,0 +1,27 @@
+public class GrapeApe extends Turtle
+{ 
+  /**
+   * Constructor that takes a model display and adds
+   * a turtle in the middle of it
+   * @param display the model display
+   */
+  public GrapeApe(World wref)
+  {
+    super(wref);
+  }
+  {
+     ex1.setShowInfo(true);
+  }
+  
+  /**
+   * Constructor that takes the x and y position and the
+   * model displayer
+   * @param x the x pos
+   * @param y the y pos
+   * @param display the model display
+   */
+  public GrapeApe(int x, int y, World wref)
+  {
+    super(x, y, wref);
+  }
+}
